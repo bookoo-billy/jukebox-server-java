c21c359de51c4d18b37d04fdec26397fb42c42a7
Bookoo Billy <bookoo.billy@gmail.com>
2019-11-16 17:23:29 +0000

    Cleaned up import statements

--- src/main/java/com/bookoo/jukeboxserver/albumdetails/AlbumGraphQLDataMutators.java
+++ src/main/java/com/bookoo/jukeboxserver/albumdetails/AlbumGraphQLDataMutators.java
@@ -3,14 +3,11 @@ package com.bookoo.jukeboxserver.albumdetails;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.Map;
 import java.util.UUID;
 
 import com.bookoo.jukeboxserver.config.Config;
 import com.bookoo.jukeboxserver.domain.Album;
-import com.bookoo.jukeboxserver.domain.Artist;
-import com.bookoo.jukeboxserver.domain.Song;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
