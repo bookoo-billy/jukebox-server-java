b369e6fd7592aa3183c35c4111a5ac058fd4d14c
Bookoo Billy <bookoo.billy@gmail.com>
2019-11-24 22:58:18 +0000

    Added corsFilter bean (not for production use)

--- src/main/java/com/bookoo/jukeboxserver/config/Config.java
+++ src/main/java/com/bookoo/jukeboxserver/config/Config.java
@@ -3,11 +3,16 @@ package com.bookoo.jukeboxserver.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @PropertySource("classpath:application.properties")
@@ -25,5 +30,18 @@ public class Config {
     @Bean
     public Connection dbConnection() throws SQLException {
         return DriverManager.getConnection(postgresDbUrl, postgresDbUser, postgresDbPassword);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list  of allowed origins
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
